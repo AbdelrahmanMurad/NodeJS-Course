94bb0ebb250a00e22ae88b58b00675199f410096
AbdelrahmanMurad <awmurad2001@gmail.com>
2023-06-05 06:28:48 +0300

    NPM Modules changes

--- 04 - Modules/03 - NPM Modules/(2) Project/02 - app.js
+++ 04 - Modules/03 - NPM Modules/(2) Project/02 - app.js
@@ -1,6 +1,6 @@
 //How to import npm modules ?? answer: like core modules.
 // and you can use restructuring, like core modules. 
-// IMPORTING-REQUIRING LIKE CORE MODULES EXACTLY.
+// IMPORTING/REQUIRING LIKE CORE MODULES EXACTLY.
 
 const express = require('express');
 // const something = require('somethingPath'); // path is just for local modules.
